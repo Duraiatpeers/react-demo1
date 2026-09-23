31f4d35b3d899b42ed61d0aeb341e9198fa72f9b
Durai <duraiatpeers@gmail.com>
2026-09-23 12:16:32 +0530

    Demo

--- src/ArticleCard.jsx
+++ src/ArticleCard.jsx
@@ -1,11 +1,23 @@
-function ArticleCard({article}){
+function ArticleCard({article,onView,onLike,onFavourite}){
+
+    const {id,title,author,description,likes} = article;
+
     return (
+
         <div className='card'>
-            <h3 >{article.title}</h3>
-            <p>{article.author}</p>
-            <p>{article.description}</p>
-            <p>Likes: {article.likes}</p>
+            <h3>{title}</h3>
+            <p>{author}</p>
+            <p>{description}</p>
+            <p>Likes: {likes}</p>
+
+            <div className='btn-row'>
+                <button onClick={(event) => onView(id)}>View</button>
+                <button onClick={(event) => onLike(id)}>Like</button>
+                <button onClick={(event) => onFavourite(id)}>Favourite</button>
+            </div>
+
         </div>
+
     )
 
 }
--- src/ArticleList.jsx
+++ src/ArticleList.jsx
@@ -1,15 +1,36 @@
 import {articles} from '../data/articles'
 import ArticleCard from './ArticleCard.jsx'
 
+
+/* ArticleList component 
+Data can be passed through props, destructuring props, object */
+
 function ArticleList(){
 
+    const handleView = (id,event)  => {
+        console.log("View article with id: ", id, ' event: ', event)
+    }
+
+    const handleLike = (id,event)  => {
+        console.log("Like article with id: ", id,' event: ', event)
+    }
+
+    const handleFavourite = (id,event)  => {
+        console.log("Favourite article with id: ", id,' event: ', event)
+    }
 
     return (
 
         <div className="article-grid" >
                 {articles.map ( 
                     article =>
-                        <ArticleCard key={article.id} article={article} /> 
+                        <ArticleCard 
+                            key={article.id} 
+                            article={article} 
+                            onView={handleView}
+                            onLike={handleLike}
+                            onFavourite={handleFavourite}
+                        /> 
                 )
                 }
             
